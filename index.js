f92b9f5c7c51372eb42a153f6bb5cf252db1252e
JudithGrau <judith.betina.grau@gmail.com>
2022-01-16 23:32:52 -0300

    Se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,21 @@
+const btnStartRecord = document.getElementById('btnStartRecord');
+const btnStopRecord = document.getElementById('btnStopRecord');
+const texto = document.getElementById('texto');
+
+let recognition = new webkitSpeechRecognition();
+recognition.lang = 'es-ES';
+recognition.continuous = true;
+recognition.interimResults = false;
+
+recognition.onresult = (event) => {
+    const results = event.results;
+    console.log(results);
+}
+
+btnStartRecord.addEventListener('click', () => {
+    recognition.start();
+});
+
+btnStopRecord.addEventListener('click', () => {
+    recognition.abort();
+});
